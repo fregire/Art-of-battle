0c66c33806f126b3f8cc042b9ae7014253e5ccf0
Daniil Gilmutdinov <fregire@mail.ru>
2020-04-13 20:36:16 +0500

    Added model of Game

---
+++ Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Art_of_battle
+{
+    class Game
+    {
+        public void Start()
+        {
+
+        }
+    }
+}
