5e8e8189282d2321a13345124ac2ed960b3b95df
Daniil Gilmutdinov <fregire@mail.ru>
2020-05-07 00:33:14 +0500

    Added Romchik changes

---
+++ View/MainForm.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Art_of_battle.View
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            ShowStartScreen();
+        }
+
+        private void ShowStartScreen()
+        {
+            HideScreens();
+            startControl.Show();
+        }
+
+        private void HideScreens()
+        {
+            startControl.Hide();
+        }
+    }
+
+    public class OptionsForm : Form
+    {
+        public OptionsForm()
+        {
+            var backToMainMenuFromOptions = new Button
+            {
+                Text = "Назад"
+            };
+            Controls.Add(backToMainMenuFromOptions);
+
+            var soundTextBox = new TextBox
+            {
+                Location = new Point(),//set x set y
+                Size = new Size()//set x set y
+            };
+            var soundTrackBar = new TrackBar
+            {
+                Minimum = 0,
+                Maximum = 100,
+                TickFrequency = 1
+            };
+            Controls.AddRange(new Control[] { soundTextBox, soundTrackBar });
+            soundTrackBar.Scroll += new EventHandler(SoundTrackBarScroll);
+
+            void SoundTrackBarScroll(object sender, EventArgs e)
+            {
+                soundTextBox.Text = "" + soundTrackBar.Value;
+            }
+        }
+
+    }
+}
